51ce93085a7d36d2c2d51bab13ed7e8b09c2e9d9
Gulrez Alam <egulrezalam@gmail.com>
2023-07-23 19:04:49 +0530

    updated

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import index, detail, vote, results
+# from .views import index, detail, vote, results
+from .views import IndexView, DetailView, ResultsView, vote
 
 app_name = 'polls'
 
